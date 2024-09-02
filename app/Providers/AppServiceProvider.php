7a5dc6eeb4ce85de59abc47878c5930107d2a235
rafaela <rafaelajtv@outlook.com>
2024-09-02 20:08:30 -0300

    views e blade adicionados

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,15 +1,22 @@
 <?php
 
+
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
+
 {
+
     /**
+
      * Register any application services.
      */
+
     public function register(): void
+
     {
         //
     }
@@ -17,8 +24,11 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+
     public function boot(): void
+
     {
-        //
+        Paginator::useBootstrap();
     }
+
 }
